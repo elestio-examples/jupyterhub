9d3f30ad64e81b241d0176cf997b587686050099
siddhantprateek <siddhantprateek@gmail.com>
2024-08-29 17:15:47 +0530

    jupyterhub ci/cd for elestio

---
+++ README.md
@@ -0,0 +1,16 @@
+# JupyterHub CI/CD pipeline
+
+<a href="https://dash.elest.io/deploy?source=cicd&social=dockerCompose&url=https://github.com/elestio-examples/jupyterhub"><img src="deploy-on-elestio.png" alt="Deploy on Elest.io" width="180px" /></a>
+
+Deploy JupyterHub server with CI/CD on Elestio
+
+<img src="jupyterhub.png" style='width: 30%;'/>
+<br/>
+<br/>
+
+# Once deployed ...
+
+You can open JupyterHub UI here:
+
+    URL: https://[CI_CD_DOMAIN]
+    password: [ADMIN_PASSWORD]
---
+++ scripts/preInstall.sh
@@ -0,0 +1,65 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+cat << EOT >> ./jupyterhub_config.py
+# Configuration file for JupyterHub
+import os
+
+c = get_config()  # noqa: F821
+
+c.JupyterHub.ip = '0.0.0.0'
+# We rely on environment variables to configure JupyterHub so that we
+# avoid having to rebuild the JupyterHub container every time we change a
+# configuration parameter.
+
+# Spawn single-user servers as Docker containers
+c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
+
+# Spawn containers from this image
+c.DockerSpawner.image = os.environ["DOCKER_NOTEBOOK_IMAGE"]
+
+# Connect containers to this Docker network
+network_name = os.environ["DOCKER_NETWORK_NAME"]
+c.DockerSpawner.use_internal_ip = True
+c.DockerSpawner.network_name = network_name
+
+# Explicitly set notebook directory because we'll be mounting a volume to it.
+# We follow the same convention.
+notebook_dir = os.environ.get("DOCKER_NOTEBOOK_DIR", "/home/jovyan/work")
+c.DockerSpawner.notebook_dir = notebook_dir
+
+# Mount the real user's Docker volume on the host to the notebook user's
+# notebook directory in the container
+c.DockerSpawner.volumes = {"jupyterhub-{username}": notebook_dir}
+
+# Privlages
+c.DockerSpawner.extra_create_kwargs = {'user': '0'}
+c.DockerSpawner.extra_host_config = {'privileged': True}
+
+# Remove containers once they are stopped
+c.DockerSpawner.remove = True
+
+# For debugging arguments passed to spawned containers
+c.DockerSpawner.debug = True
+
+# User containers will access hub by container name on the Docker network
+c.JupyterHub.hub_ip = "jupyterhub"
+c.JupyterHub.hub_port = 8080
+
+# Persist hub data on volume mounted inside container
+c.JupyterHub.cookie_secret_file = "/data/jupyterhub_cookie_secret"
+c.JupyterHub.db_url = "sqlite:////data/jupyterhub.sqlite"
+
+# Allow all signed-up users to login
+c.Authenticator.allow_all = True
+c.JupyterHub.authenticator_class = 'jupyterhub.auth.PAMAuthenticator'
+c.PAMAuthenticator.admin_users = {'admin'}
+
+from jupyterhub.auth import DummyAuthenticator
+c.JupyterHub.authenticator_class = DummyAuthenticator
+c.DummyAuthenticator.password = '$ADMIN_PASSWORD'
+
+admin = os.environ.get("JUPYTERHUB_ADMIN")
+if admin:
+    c.Authenticator.admin_users = [admin]
+EOT
